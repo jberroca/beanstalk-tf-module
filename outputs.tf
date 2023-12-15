dea75914d7b22942df5a67e2e7c62256496e711c
jberroca <berrocaljo@gmail.com>
2023-12-15 02:03:43 -0300

    add variables.tf and outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "Beanstalk_application_arn" {
+  value       = module.bice_beanstalk_module.Beanstalk_application_arn
+  description = "Created Elastic Beanstalk application arn"
+
+}
+
+output "beanstalk_environment_arn" {
+  value       = module.bice_beanstalk_module.beanstalk_environment_arn
+  description = "created Elastic Beanstalk environment arn"
+}
