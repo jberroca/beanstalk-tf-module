f68431c2652d3d64c8f58a46d5d0351e41a8e2c9
jberroca <berrocaljo@gmail.com>
2023-12-14 22:27:47 -0300

    add comment

--- beanstalk/main.tf
+++ beanstalk/main.tf
@@ -7,9 +7,10 @@ resource "aws_elastic_beanstalk_environment" "bice_beanstalk_environment" {
   name                = var.bice_environment
   application         = aws_elastic_beanstalk_application.bice_beanstalk_application.name
   solution_stack_name = var.bice_solution_stack_name
+  # Settings for the environment
   setting {
     namespace = "aws:ec2:vpc"
     name      = "VPCId"
-    value     = var.bice_vpc_id # Specify the ID of your VPC
+    value     = var.bice_vpc_id 
   }
 }
